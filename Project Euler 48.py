64a9432760cbaea8e014add89700e0e9458fddba
edmataochoa <emusicals@hotmail.com>
2017-07-24 11:30:50 -0700

    +

--- Project Euler 48.py
+++ Project Euler 48.py
@@ -2,7 +2,6 @@ def add_powers(max_num):
     sum_powers = 0
     for n in range(1, max_num + 1):
         sum_powers += (n ** n)
-        n += 1
     return sum_powers
 
 def last_digits(number, num_of_digits):
