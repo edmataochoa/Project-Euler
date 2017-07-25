84855f0210514e58bc817176f2c980e896724373
edmataochoa <emusicals@hotmail.com>
2017-07-24 17:24:06 -0700

    +

--- Project Euler 13.py
+++ Project Euler 13.py
@@ -2,8 +2,12 @@ large_num = open("p013_large_num.txt", "r").read()
 
 def first_digits_sum(num):
     total = 0
+    current_num = ""
     for digit in str(num):
-        total += int(digit)
+        current_num += digit
+        if digit == "\n":
+            total += int(current_num)
+            current_num = ""
 
     def how_many_digits(quantity):
         return str(total)[0:quantity]
