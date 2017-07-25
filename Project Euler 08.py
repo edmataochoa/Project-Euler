788ff939af2836272a62e3f48b19ca311924cd68
edmataochoa <emusicals@hotmail.com>
2017-07-24 19:41:09 -0700

    +

---
+++ Project Euler 08.py
@@ -0,0 +1,11 @@
+numbers = open("p008_numbers.txt", "r").read()
+
+def get_numbers(number):
+    return [int(n) for n in number if n.isdigit()]
+
+def multiply_quantity(num_of_digits, number):
+    return
+
+
+multilply_thirteen = multiply_quantity(13)
+print(get_numbers(numbers))
