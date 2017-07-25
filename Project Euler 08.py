cd93f600650b2be0d8cb0c63891b8953af504e52
edmataochoa <emusicals@hotmail.com>
2017-07-24 19:55:00 -0700

    +

--- Project Euler 08.py
+++ Project Euler 08.py
@@ -1,11 +1,24 @@
 numbers = open("p008_numbers.txt", "r").read()
 
-def get_numbers(number):
-    return [int(n) for n in number if n.isdigit()]
+def multiply_quantity(number):
+    list_numbers = [int(n) for n in number if n.isdigit()]
 
-def multiply_quantity(num_of_digits, number):
-    return
+    def get_numbers(num_of_digits):
+        count = 0
+        total = 0
+        current_num = 0
+        for n in list_numbers:
+            while count < num_of_digits:
+                total *= n
+                count += 1
+            if total > current_num:
+                current_num = total
+            count = 0
+            total = 0
+        return current_num
+
+    return get_numbers
 
 
-multilply_thirteen = multiply_quantity(13)
-print(get_numbers(numbers))
+multilply_numbers = multiply_quantity(numbers)
+print(multilply_numbers(4))
